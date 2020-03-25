d78f3a98bef4a0e53b2365ec63672c0777a068a3
nariakiiwatani <nariakiiwatani@annolab.com>
2020-03-25 16:24:10 +0900

    re-fixed

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -9,9 +9,9 @@ void ofApp::setup(){
 	// you can specify default argument(s)
 	prompt_.subscribe("str", [](std::string s) { cout << s << endl; }, {"hoge"});
 	// many parameters...
-	prompt_.subscribe("many", [](int _0, int _1, int _2, int _3, int _4, int _5, int _6, int _7) {
+	prompt_.subscribe("many", std::function<void(int,int,int,int,int,int,int,int)>([](int _0, int _1, int _2, int _3, int _4, int _5, int _6, int _7) {
 		cout << _0 << " " << _1 << " " << _2 << " " << _3 << " " << _4 << " " << _5 << " " << _6 << " " << _7 << endl;
-	}, {0,1,2,3,4,5,6,7});
+	}), {0,1,2,3,4,5,6,7});
 	// member function
 	prompt_.subscribe("some", this, &ofApp::someFunction);
 	// member function with default arguments
